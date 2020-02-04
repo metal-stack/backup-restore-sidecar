e7efc3a202b44fe311a33e100a3079d191611828
Gerrit <gerrit.schwerthelm@f-i-ts.de>
2020-02-04 23:32:47 +0100

    Remove unused error package.

--- cmd/internal/backup/errors/errors.go
+++
@@ -1,8 +0,0 @@
-package errors
-
-// NoBackupsAvailableError indicates that no backups for the database exist in the cloud provider bucket
-type NoBackupsAvailableError struct{}
-
-func (e NoBackupsAvailableError) Error() string {
-	return "no backups available"
-}
