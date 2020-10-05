930d86d5a4861961ebaa0e71f2518947753d5a24
Stefan Majer <stefan.majer@f-i-ts.de>
2020-10-05 09:15:25 +0200

    fix lint errors in tests

--- cmd/internal/encryption/encryption_test.go
+++ cmd/internal/encryption/encryption_test.go
@@ -11,21 +11,24 @@ import (
 
 func TestEncrypter(t *testing.T) {
 	// Key too short
-	e, err := New(zap.L().Sugar(), "tooshortkey")
+	_, err := New(zap.L().Sugar(), "tooshortkey")
 	assert.EqualError(t, err, "key length:11 invalid, must be 16,24 or 32 bytes")
 
 	// Key too short
-	e, err = New(zap.L().Sugar(), "19bytesofencryption")
+	_, err = New(zap.L().Sugar(), "19bytesofencryption")
 	assert.EqualError(t, err, "key length:19 invalid, must be 16,24 or 32 bytes")
 
 	// Key too long
-	e, err = New(zap.L().Sugar(), "tooloooonoooooooooooooooooooooooooooongkey")
+	_, err = New(zap.L().Sugar(), "tooloooonoooooooooooooooooooooooooooongkey")
 	assert.EqualError(t, err, "key length:42 invalid, must be 16,24 or 32 bytes")
 
-	e, err = New(zap.L().Sugar(), "01234567891234560123456789123456")
+	e, err := New(zap.L().Sugar(), "01234567891234560123456789123456")
 	assert.NoError(t, err, "")
 
 	input, err := ioutil.TempFile("", "encrypt")
+	assert.NoError(t, err)
+	defer os.Remove(input.Name())
+
 	cleartextInput := []byte("This is the content of the file")
 	err = ioutil.WriteFile(input.Name(), cleartextInput, 0644)
 	assert.NoError(t, err)
@@ -42,7 +45,8 @@ func TestEncrypter(t *testing.T) {
 
 	// Test with 100MB file
 	bigBuff := make([]byte, 100000000)
-	ioutil.WriteFile("bigfile.test", bigBuff, 0666)
+	err = ioutil.WriteFile("bigfile.test", bigBuff, 0666)
+	assert.NoError(t, err)
 
 	bigEncFile, err := e.Encrypt("bigfile.test")
 	assert.NoError(t, err)
