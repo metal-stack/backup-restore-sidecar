536fd77426e46a0ca1fd69d7f8f806551c5d274b
Gerrit <info@gerritschwerthelm.de>
2023-08-25 18:25:27 +0200

    Add postgres skeleton.

--- integration/postgres_test.go
+++ integration/postgres_test.go
@@ -2,38 +2,28 @@ package integration_test
 
 import (
 	"context"
-	"fmt"
 	"testing"
 
 	"github.com/metal-stack/backup-restore-sidecar/pkg/constants"
 	"github.com/metal-stack/metal-lib/pkg/pointer"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-
-	"github.com/avast/retry-go/v4"
-	r "gopkg.in/rethinkdb/rethinkdb-go.v6"
 )
 
 const (
-	rethinkDbContainerImage = "rethinkdb:2.4.0"
+	postgresContainerImage = "postgres:15-alpine"
 )
 
-func Test_RethinkDB(t *testing.T) {
-	type testData struct {
-		ID   string `rethinkdb:"id"`
-		Data string `rethinkdb:"data"`
-	}
-
+func Test_Postgres(t *testing.T) {
 	const (
-		rethinkdbPassword = "test123!"
-		db                = "backup-restore"
-		table             = "precioustestdata"
+		db               = "postgres"
+		postgresPassword = "test123!"
+		postgresUser     = "test"
+		table            = "precioustestdata"
 	)
 
 	var (
@@ -44,52 +34,104 @@ func Test_RethinkDB(t *testing.T) {
 					APIVersion: appsv1.SchemeGroupVersion.String(),
 				},
 				ObjectMeta: metav1.ObjectMeta{
-					Name:      "rethinkdb",
+					Name:      "postgres",
 					Namespace: namespace,
 					Labels: map[string]string{
-						"app": "rethinkdb",
+						"app": "postgres",
 					},
 				},
 				Spec: appsv1.StatefulSetSpec{
-					ServiceName: "rethinkdb",
+					ServiceName: "postgres",
 					Replicas:    pointer.Pointer(int32(1)),
 					Selector: &metav1.LabelSelector{
 						MatchLabels: map[string]string{
-							"app": "rethinkdb",
+							"app": "postgres",
 						},
 					},
 					Template: corev1.PodTemplateSpec{
 						ObjectMeta: metav1.ObjectMeta{
 							Labels: map[string]string{
-								"app": "rethinkdb",
+								"app": "postgres",
 							},
 						},
 						Spec: corev1.PodSpec{
 							HostNetwork: true,
 							Containers: []corev1.Container{
 								{
-									Name:    "rethinkdb",
-									Image:   rethinkDbContainerImage,
+									Name:    "postgres",
+									Image:   postgresContainerImage,
 									Command: []string{"backup-restore-sidecar", "wait"},
+									LivenessProbe: &corev1.Probe{
+										ProbeHandler: corev1.ProbeHandler{
+											Exec: &corev1.ExecAction{
+												Command: []string{"/bin/sh", "-c", "exec", "pg_isready", "-U", postgresUser, "-h", "127.0.0.1", "-p", "5432"},
+											},
+										},
+										InitialDelaySeconds: 30,
+										TimeoutSeconds:      5,
+										PeriodSeconds:       10,
+										SuccessThreshold:    1,
+										FailureThreshold:    6,
+									},
+									ReadinessProbe: &corev1.Probe{
+										ProbeHandler: corev1.ProbeHandler{
+											Exec: &corev1.ExecAction{
+												Command: []string{"/bin/sh", "-c", "exec", "pg_isready", "-U", postgresUser, "-h", "127.0.0.1", "-p", "5432"},
+											},
+										},
+										InitialDelaySeconds: 10,
+										TimeoutSeconds:      5,
+										PeriodSeconds:       10,
+									},
 									Env: []corev1.EnvVar{
 										{
-											Name: "RETHINKDB_PASSWORD",
+											Name: "POSTGRES_DB",
 											ValueFrom: &corev1.EnvVarSource{
 												SecretKeyRef: &corev1.SecretKeySelector{
 													LocalObjectReference: corev1.LocalObjectReference{
-														Name: "rethinkdb",
+														Name: "postgres",
 													},
-													Key: "rethinkdb-password",
+													Key: "POSTGRES_DB",
+												},
+											},
+										},
+										{
+											Name: "POSTGRES_USER",
+											ValueFrom: &corev1.EnvVarSource{
+												SecretKeyRef: &corev1.SecretKeySelector{
+													LocalObjectReference: corev1.LocalObjectReference{
+														Name: "postgres",
+													},
+													Key: "POSTGRES_USER",
+												},
+											},
+										},
+										{
+											Name: "POSTGRES_PASSWORD",
+											ValueFrom: &corev1.EnvVarSource{
+												SecretKeyRef: &corev1.SecretKeySelector{
+													LocalObjectReference: corev1.LocalObjectReference{
+														Name: "postgres",
+													},
+													Key: "POSTGRES_PASSWORD",
+												},
+											},
+										},
+										{
+											Name: "PGDATA",
+											ValueFrom: &corev1.EnvVarSource{
+												SecretKeyRef: &corev1.SecretKeySelector{
+													LocalObjectReference: corev1.LocalObjectReference{
+														Name: "postgres",
+													},
+													Key: "POSTGRES_DATA",
 												},
 											},
 										},
 									},
 									Ports: []corev1.ContainerPort{
 										{
-											ContainerPort: 8080,
-										},
-										{
-											ContainerPort: 28015,
+											ContainerPort: 5432,
 										},
 									},
 									VolumeMounts: []corev1.VolumeMount{
@@ -110,8 +152,32 @@ func Test_RethinkDB(t *testing.T) {
 								},
 								{
 									Name:    "backup-restore-sidecar",
-									Image:   rethinkDbContainerImage,
+									Image:   postgresContainerImage,
 									Command: []string{"backup-restore-sidecar", "start", "--log-level=debug"},
+									Env: []corev1.EnvVar{
+										{
+											Name: "BACKUP_RESTORE_SIDECAR_POSTGRES_PASSWORD",
+											ValueFrom: &corev1.EnvVarSource{
+												SecretKeyRef: &corev1.SecretKeySelector{
+													LocalObjectReference: corev1.LocalObjectReference{
+														Name: "postgres",
+													},
+													Key: "POSTGRES_PASSWORD",
+												},
+											},
+										},
+										{
+											Name: "BACKUP_RESTORE_SIDECAR_POSTGRES_USER",
+											ValueFrom: &corev1.EnvVarSource{
+												SecretKeyRef: &corev1.SecretKeySelector{
+													LocalObjectReference: corev1.LocalObjectReference{
+														Name: "postgres",
+													},
+													Key: "POSTGRES_USER",
+												},
+											},
+										},
+									},
 									Ports: []corev1.ContainerPort{
 										{
 											Name:          "grpc",
@@ -128,10 +194,6 @@ func Test_RethinkDB(t *testing.T) {
 											MountPath: "/data",
 										},
 										{
-											Name:      "rethinkdb-credentials",
-											MountPath: "/rethinkdb-secret",
-										},
-										{
 											Name:      "backup-restore-sidecar-config",
 											MountPath: "/etc/backup-restore-sidecar",
 										},
@@ -139,16 +201,6 @@ func Test_RethinkDB(t *testing.T) {
 											Name:      "bin-provision",
 											SubPath:   "backup-restore-sidecar",
 											MountPath: "/usr/local/bin/backup-restore-sidecar",
-										},
-										{
-											Name:      "bin-provision",
-											SubPath:   "rethinkdb-dump",
-											MountPath: "/usr/local/bin/rethinkdb-dump",
-										},
-										{
-											Name:      "bin-provision",
-											SubPath:   "rethinkdb-restore",
-											MountPath: "/usr/local/bin/rethinkdb-restore",
 										},
 									},
 								},
@@ -161,8 +213,6 @@ func Test_RethinkDB(t *testing.T) {
 									Command: []string{
 										"cp",
 										"/backup-restore-sidecar",
-										"/rethinkdb/rethinkdb-dump",
-										"/rethinkdb/rethinkdb-restore",
 										"/bin-provision",
 									},
 									VolumeMounts: []corev1.VolumeMount{
@@ -191,25 +241,11 @@ func Test_RethinkDB(t *testing.T) {
 									},
 								},
 								{
-									Name: "rethinkdb-credentials",
-									VolumeSource: corev1.VolumeSource{
-										Secret: &corev1.SecretVolumeSource{
-											SecretName: "rethinkdb",
-											Items: []corev1.KeyToPath{
-												{
-													Key:  "rethinkdb-password",
-													Path: "rethinkdb-password.txt",
-												},
-											},
-										},
-									},
-								},
-								{
 									Name: "backup-restore-sidecar-config",
 									VolumeSource: corev1.VolumeSource{
 										ConfigMap: &corev1.ConfigMapVolumeSource{
 											LocalObjectReference: corev1.LocalObjectReference{
-												Name: "backup-restore-sidecar-config-rethinkdb",
+												Name: "backup-restore-sidecar-config-postgres",
 											},
 										},
 									},
@@ -267,21 +303,20 @@ func Test_RethinkDB(t *testing.T) {
 						APIVersion: corev1.SchemeGroupVersion.String(),
 					},
 					ObjectMeta: metav1.ObjectMeta{
-						Name:      "backup-restore-sidecar-config-rethinkdb",
+						Name:      "backup-restore-sidecar-config-postgres",
 						Namespace: namespace,
 					},
 					Data: map[string]string{
 						"config.yaml": `---
 bind-addr: 0.0.0.0
-db: rethinkdb
-db-data-directory: /data/rethinkdb/
+db: postgres
+db-data-directory: /data/postgres/
 backup-provider: local
-rethinkdb-passwordfile: /rethinkdb-secret/rethinkdb-password.txt
 backup-cron-schedule: "*/1 * * * *"
-object-prefix: rethinkdb-test
+object-prefix: postgres-test
+compression-method: tar
 post-exec-cmds:
-# IMPORTANT: the --directory needs to point to the exact sidecar data dir, otherwise the database will be restored to the wrong location
-- rethinkdb --bind all --directory /data/rethinkdb --initial-password ${RETHINKDB_PASSWORD}
+- docker-entrypoint.sh postgres
 `,
 					},
 				},
@@ -291,11 +326,14 @@ post-exec-cmds:
 						APIVersion: corev1.SchemeGroupVersion.String(),
 					},
 					ObjectMeta: metav1.ObjectMeta{
-						Name:      "rethinkdb",
+						Name:      "postgres",
 						Namespace: namespace,
 					},
 					StringData: map[string]string{
-						"rethinkdb-password": rethinkdbPassword,
+						"POSTGRES_DB":       db,
+						"POSTGRES_USER":     postgresUser,
+						"POSTGRES_PASSWORD": postgresPassword,
+						"POSTGRES_DATA":     "/data/postgres/",
 					},
 				},
 				&corev1.Service{
@@ -304,26 +342,21 @@ post-exec-cmds:
 						APIVersion: corev1.SchemeGroupVersion.String(),
 					},
 					ObjectMeta: metav1.ObjectMeta{
-						Name:      "rethinkdb",
+						Name:      "postgres",
 						Namespace: namespace,
 						Labels: map[string]string{
-							"app": "rethinkdb",
+							"app": "postgres",
 						},
 					},
 					Spec: corev1.ServiceSpec{
 						Selector: map[string]string{
-							"app": "rethinkdb",
+							"app": "postgres",
 						},
 						Ports: []corev1.ServicePort{
 							{
-								Name:       "10080",
-								Port:       10080,
-								TargetPort: intstr.FromInt32(10080),
-							},
-							{
-								Name:       "28015",
-								Port:       28015,
-								TargetPort: intstr.FromInt32(28015),
+								Name:       "5432",
+								Port:       5432,
+								TargetPort: intstr.FromInt32(5432),
 							},
 							{
 								Name:       "metrics",
@@ -336,78 +369,17 @@ post-exec-cmds:
 			}
 		}
 
-		newRethinkdbSession = func(t *testing.T, ctx context.Context) *r.Session {
-			var session *r.Session
-			err := retry.Do(func() error {
-				var err error
-				session, err = r.Connect(r.ConnectOpts{
-					Addresses: []string{"localhost:28015"},
-					Database:  db,
-					Username:  "admin",
-					Password:  rethinkdbPassword,
-					MaxIdle:   10,
-					MaxOpen:   20,
-				})
-				if err != nil {
-					return fmt.Errorf("cannot connect to DB: %w", err)
-				}
-
-				return nil
-			}, retry.Context(ctx))
-			require.NoError(t, err)
-
-			return session
-		}
-
 		addTestData = func(t *testing.T, ctx context.Context) {
-			session := newRethinkdbSession(t, ctx)
-
-			_, err := r.DBCreate(db).RunWrite(session)
-			require.NoError(t, err)
-
-			_, err = r.DB(db).TableCreate(table).RunWrite(session)
-			require.NoError(t, err)
-
-			_, err = r.DB(db).Table(table).Insert(testData{
-				ID:   "1",
-				Data: "i am precious",
-			}).RunWrite(session)
-			require.NoError(t, err)
-
-			cursor, err := r.DB(db).Table(table).Get("1").Run(session)
-			require.NoError(t, err)
-
-			var d1 testData
-			err = cursor.One(&d1)
-			require.NoError(t, err)
-			require.Equal(t, "i am precious", d1.Data)
+			// TODO: Implement
 		}
 
 		verifyTestData = func(t *testing.T, ctx context.Context) {
-			session := newRethinkdbSession(t, ctx)
-
-			var d2 testData
-			err := retry.Do(func() error {
-				cursor, err := r.DB(db).Table(table).Get("1").Run(session)
-				if err != nil {
-					return err
-				}
-
-				err = cursor.One(&d2)
-				if err != nil {
-					return err
-				}
-
-				return nil
-			})
-			require.NoError(t, err)
-
-			assert.Equal(t, "i am precious", d2.Data)
+			// TODO: Implement
 		}
 	)
 
 	restoreFlow(t, &flowSpec{
-		databaseType:     "rethinkdb",
+		databaseType:     "postgres",
 		sts:              sts,
 		backingResources: backingResources,
 		addTestData:      addTestData,
