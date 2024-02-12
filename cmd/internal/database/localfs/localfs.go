3d32606f513ed6e43d4861025e64abce101c7471
qrnvttrl <quirin.vetterl@gmx.de>
2024-02-12 09:04:58 +0100

    Implemented localfs

--- cmd/internal/database/localfs/localfs.go
+++ cmd/internal/database/localfs/localfs.go
@@ -2,12 +2,24 @@ package localfs
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
+	"os"
+
+	"github.com/metal-stack/backup-restore-sidecar/cmd/internal/utils"
+	"github.com/metal-stack/backup-restore-sidecar/pkg/constants"
+	"github.com/spf13/afero"
+)
+
+const (
+	backupDest = constants.BackupDir + "/localfs"
+	restoreSrc = constants.RestoreDir + "/localfs"
 )
 
 type LocalFS struct {
-	datadir string
-	log     *slog.Logger
+	datadir   string
+	fileNames []string
+	log       *slog.Logger
 }
 
 func New(log *slog.Logger, datadir string) *LocalFS {
@@ -17,18 +29,66 @@ func New(log *slog.Logger, datadir string) *LocalFS {
 	}
 }
 
+// Check if Datadir contains the specified filenames
 func (l *LocalFS) Check(ctx context.Context) (bool, error) {
-	//ToDo: check if Datadir empty -> true
-	return true, nil
+	dirEntrys, err := os.ReadDir(l.datadir)
+
+	if err != nil {
+		return false, err
+	}
+
+	result := true
+
+	for i := 0; i < len(l.fileNames) && result; i++ {
+		found := false
+
+		for j := 0; j < len(dirEntrys) && !found; j++ {
+			curEntry, err := dirEntrys[j].Info()
+
+			if err != nil {
+				return false, err
+			}
+
+			found = l.fileNames[i] == curEntry.Name()
+		}
+
+		result = found
+	}
+
+	return result, nil
 }
 
+// put Datadir into constants.Backupdir directory
 func (l *LocalFS) Backup(ctx context.Context) error {
 	//ToDo: put Datadir into compressed archive
+
+	if err := os.RemoveAll(constants.BackupDir); err != nil {
+		return fmt.Errorf("could not clean backup directory: %w", err)
+	}
+
+	if err := os.MkdirAll(constants.BackupDir, 0777); err != nil {
+		return fmt.Errorf("could not create backup directory: %w", err)
+	}
+
+	if err := utils.Copy(afero.NewOsFs(), l.datadir, backupDest); err != nil {
+		return fmt.Errorf("could not copy contents: %w", err)
+	}
+
+	l.log.Debug("Sucessfully took backup of localfs")
 	return nil
 }
 
 func (l *LocalFS) Recover(ctx context.Context) error {
 	//ToDo: decompress archive into Datadir
+	if err := utils.RemoveContents(l.datadir); err != nil {
+		return fmt.Errorf("Could not cleanup Datadir: %w", err)
+	}
+
+	if err := utils.Copy(afero.NewOsFs(), restoreSrc, l.datadir); err != nil {
+		return fmt.Errorf("could not copy contents: %w", err)
+	}
+
+	l.log.Debug("Successfully restored localfs")
 	return nil
 }
 
